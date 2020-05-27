ea9a23317ad0f5b8daa4a5a5a3c31b7a8e93d693
6donote4 <6donote4@gmail.com>
2020-05-27 21:14:48 +0800

    revise cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
- cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Release
+ #cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Release
+ cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=cmake/linux-compiler-i386-multilib.cmake
  make
  make install
 
