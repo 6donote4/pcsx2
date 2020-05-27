036cb73b5009d40d30a62e0475b1af62ad855942
6donote4 <6donote4@gmail.com>
2020-05-27 22:34:32 +0800

    add ps2 bios

--- cmake.sh
+++ cmake.sh
@@ -2,6 +2,6 @@
  #cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Release
  #git checkout 56b76b1 --pcsx2 ver 1.6
  cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=cmake/linux-compiler-i386-multilib.cmake
- make
+ make -j4
  make install
 
