b534e306536604a79c1dcf67bf881c10f800ed77
6donote4 <6donote4@gmail.com>
2020-05-27 23:19:03 +0800

    revise cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  #cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Release
  #git checkout 56b76b1 --pcsx2 ver 1.6
- #git switch 56b76b1 --pcsx2 ver 1.6
+ #git switch -c 56b76b1 --pcsx2 ver 1.6
  cmake CMakeLists.txt -DCMAKE_TOOLCHAIN_FILE=cmake/linux-compiler-i386-multilib.cmake
  make -j4
  make install
